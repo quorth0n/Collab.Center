2ab2981e9a83b8538ddb2595995270e89710408f
Mulletfingers999 <lpoflynn@gmail.com>
2015-06-17 18:16:36 -0700

    A sincere message for /support/

--- support/index.php
+++ support/index.php
@@ -27,7 +27,7 @@
     font-size: 20px;
     font-weight: lighter;
     text-align: center;
-    margin: 5em;
+    margin: 2em;
   }
   h1 {
     font-size: 40px;
@@ -43,9 +43,36 @@
 </head>
 <body>
   <div id="main">
-    <h1>Nothing to see here... yet</h1>
+    <h1>So, you're looking to help support collab.center?</h1>
     <hr/>
-    <h2>As we actively develop Collab.Center, we have bigger and better features to add, but don't fret, this will be coming right up soon!<br/> Check back often!</h2>
+    <h2>We can't thank you enough.</h2>
+    <h3>The project was started by a small 11-year-old year old 'coder' as a request of his dad, but since has grown to be much larger, join in a partnership with EternityIncurakai, and developed a small-but-tight community of it's own down south at the <a href="https://github.com/Mulletfingers999/Collab.Center" target="_blank">GitHub repo</a>.</h3>
+    <hr/>
+    <h3>Liking our Facebook or LinkedIn Page is quick, easy, will keep you up on the latest updates, and is <strong><i>an incredible amount of help</i></strong> for such a simple task. ;)</h3>
+    <script src="//platform.linkedin.com/in.js" type="text/javascript">
+    //lang: en_US
+    </script>
+    <script type="IN/Share" data-url="collab.center" data-counter="top">
+    </script>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <iframe src="http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fcollabcentercoding&amp;width=1&amp;layout=box_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=65&amp;appId=330440663773880" style="border: 0; width:65px; height:65px;"></iframe>
+    <br/><br/><hr/>
+
+    <h3>For our <i>incredibly caring</i> users who would love to keep us ad-free, we also have two methods of donations: PayPal or Flattr</h3>
+
+    <script id="fbto2qc">(function(i){var f,s=document.getElementById(i);f=document.createElement("iframe");f.src="//api.flattr.com/button/view/?uid=poflynn&url="+encodeURIComponent(document.URL);f.title="Flattr";f.height=62;f.width=55;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})("fbto2qc");
+    </script>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <!-- paypal -->
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="display: inline; vertical-align: middle;">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAWvy8reKKNiLRLTDd/iyr7BcYUNLlNGt5N5egwX6rJO/t7e54FjxCMrUWtAlqbtxdJNuRkV7ZlbAxCj1GIA4KrDN7xsYCBuD420RKmR50m9peG2YXu8e+ehor81ed684ABHoPkBU4DLfJTlfg4++ZiBandp/oPfkH0uumxc5O/kjELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIHMlGchwAap6AgYilr6mbA39pkIY1/DVRZ43i8cj8x6liGEmipqZ6smJqm689SUCGW+Ez1M8GVP0+WSYD2GFl1boRMxNDG9tqI6zRFdr2PnHleIs4jT5bVPByEVVfYvekHOMjcCtf/N/JmgL19J0cwSExjdzmP5ldyhIvBy8JG0rs40WHCRCmRgtuZrN1P90aDXgdoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUwMTExMDI0MDE3WjAjBgkqhkiG9w0BCQQxFgQUYIdrNjOeN/3+NeCgUalAZFJuAj0wDQYJKoZIhvcNAQEBBQAEgYCAm2mD7I14ALfW708eDnyaEyhv53Ca8Aoh39juSbgnqzhzuwySa/RKF/u1ViZ3wE+gGFwfD3Fes+fspvpI4a+1mHR3VYw7/P9eHcNDcODI1pX9rVJ5c9KCJlOo9gyVGdngiSfUPTrTDX98ATD3iPNvEvEbVJ13NLtRxgOxT32DLQ==-----END PKCS7-----
+      ">
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="PayPal - Donate" style="margin-bottom: 1.5%; border: 0;">
+      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+    </form>
+    <br/><hr/>
+    <h1>Thank You! Your Contribution Really Means a Lot! :)</h1>
   </div>
   <div id="sidebar" class="section">
     <iframe style="border: 0; width: 100%; height: 100%;" src="../sidebar/?olddocs=false">ERR: Your browser does not support <code>iframes</code></iframe>
